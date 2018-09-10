63d8fe75a50a1d16ccfd264caf90d66824aa1e0f
Parvez Shaikh <prvz786123@gmail.com>
2018-09-10 23:21:20 +0530

    code formatted

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-var express=require('express');
-var bodyParser=require('body-parser');
-var {ObjectId} = require('mongodb');
+const _=require('lodash');
+const express=require('express');
+const bodyParser=require('body-parser');
+const {ObjectId} = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
 const app=express();
 const port=process.env.PORT || 3000;
@@ -73,6 +74,54 @@ app.delete('/todos/:id',(req,res)=>{
     }
   },(err)=>{
     res.status(400).send("Some error occured")
+  })
+})
+
+// app.patch('/todos/:id',(req,res)=>{
+//   let id=req.params.id;
+//   var body=_.pick(req.body,['text','completed']);
+//
+//   if(!ObjectId.isValid(id)){
+//     return res.status(400).send({})
+//   }
+//
+//   if(_.isBoolean(body.completed) && body.completed){
+//     body.completedAt = new Date().getTime();
+//   } else {
+//     body.completed=false;
+//     body.completedAt=null;
+//   }
+//   Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+//     if(!todo){
+//       return res.status(404).send();
+//     }
+//     res.send({todo});
+//   }).catch((err)=>{
+//     res.status(400).send();
+//   })
+// })
+
+app.patch('/todos/:id',(req,res)=>{
+  let id=req.params.id;
+  let body=_.pick(req.body,['text','completed'])
+
+  if(!ObjectId.isValid(id)){
+    return res.status(400).send();
+  }
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt=new Date().getTime();
+  }else {
+    body.completed=false;
+    body.completedAt=null;
+  }
+
+  Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+    if(!todo){
+      return res.status(400).send()
+    }
+    res.send({todo})
+  }).catch((err)=>{
+    res.status(400).send();
   })
 })
 
